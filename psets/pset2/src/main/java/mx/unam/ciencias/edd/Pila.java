0253c603bdb219c5b37cdc48fe3bd4715d587040
David Rivera Morales <driveramorales46@gmail.com>
2023-02-14 19:50:37 -0600

    finished pset2

---
+++ psets/pset2/src/main/java/mx/unam/ciencias/edd/Pila.java
@@ -0,0 +1,53 @@
+package mx.unam.ciencias.edd;
+
+/**
+ * Clase para pilas genéricas.
+ */
+public class Pila<T> extends MeteSaca<T> {
+
+    /**
+     * Regresa una representación en cadena de la pila.
+     * 
+     * @return una representación en cadena de la pila.
+     */
+    @Override
+    public String toString() {
+        // Aquí va su código.
+
+        String s = "";
+        MeteSaca<T> pila = new Pila<>();
+        while (!esVacia()) {
+            T elemento = saca();
+            s += String.valueOf(elemento) + "\n";
+            pila.mete(elemento);
+        }
+        while (!pila.esVacia())
+            mete(pila.saca());
+        return s;
+
+    }
+
+    /**
+     * Agrega un elemento al tope de la pila.
+     * 
+     * @param elemento el elemento a agregar.
+     * @throws IllegalArgumentException si <code>elemento</code> es
+     *                                  <code>null</code>.
+     */
+    @Override
+    public void mete(T elemento) {
+        // Aquí va su código.
+
+        if (elemento == null)
+            throw new IllegalArgumentException();
+
+        Nodo nodo = new Nodo(elemento);
+        if (cabeza == null) {
+            cabeza = nodo;
+            rabo = nodo;
+        } else {
+            nodo.siguiente = cabeza;
+            cabeza = nodo;
+        }
+    }
+}
